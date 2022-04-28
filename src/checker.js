ad1e1e99aa1d364de1ec451875ca9ff0a33a912b
ness <duranr@protonmail.com>
2022-04-27 20:10:21 -0400

    Skeleton Code

---
+++ src/checker.js
@@ -0,0 +1,18 @@
+/*
+Auth - Login using personal access token
+ */
+
+/*
+Request Video Data, Caption Data, Channel Data
+-> Fancy Math for Call Checks
+-> Database to Store Information [Video Data, toJSON Data]
+ */
+
+/*
+Index information
+ */
+
+/*
+Display
+-> Send File
+ */
---
+++ src/ig.js
@@ -0,0 +1,4 @@
+/*
+Functions for Request
+-> Webscraping
+ */
